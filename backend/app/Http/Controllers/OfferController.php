75413a2bfa2e27453c2674822db82b8ec9476a26
Djalma Manfrin <djalma.manfrin@gmail.com>
2026-04-29 22:35:26 -0300

    Adiciona `OfferController` com ação `store` e rota `/offers` para cadastro de ofertas

---
+++ backend/app/Http/Controllers/OfferController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Actions\Offer\CreateOfferAction;
+use App\Helpers\ApiResponse;
+use Illuminate\Http\Request;
+
+class OfferController extends Controller
+{
+    public function store(Request $request, CreateOfferAction $action)
+    {
+        $data = $request->validate([
+            'product_name' => 'required|string',
+            'market_name' => 'required|string',
+            'price' => 'required|numeric|min:0',
+            'barcode' => 'nullable|string'
+        ]);
+
+        $offer = $action($data);
+
+        return ApiResponse::success('Oferta cadastrada com sucesso', ['offer_id' => $offer->id]);
+    }
+}
--- backend/routes/api_v1.php
+++ backend/routes/api_v1.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\OfferController;
 use App\Http\Controllers\SearchController;
 
 Route::post('/search', [SearchController::class, 'search']);
+Route::post('/offers', [OfferController::class, 'store']);
