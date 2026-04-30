2e29864e7a7094181d593abca754690db6c18cf9
Djalma Manfrin <djalma.manfrin@gmail.com>
2026-04-30 08:39:53 -0300

    Adiciona tipagem de retorno `JsonResponse` nos métodos `search` e `store` em `SearchController` e `OfferController`

--- backend/app/Http/Controllers/OfferController.php
+++ backend/app/Http/Controllers/OfferController.php
@@ -5,10 +5,11 @@ namespace App\Http\Controllers;
 use App\Actions\Offer\CreateOfferAction;
 use App\Helpers\ApiResponse;
 use App\Http\Requests\StoreOfferRequest;
+use Illuminate\Http\JsonResponse;
 
 class OfferController extends Controller
 {
-    public function store(StoreOfferRequest $request, CreateOfferAction $action)
+    public function store(StoreOfferRequest $request, CreateOfferAction $action): JsonResponse
     {
         $offer = $action($request->validated());
         return ApiResponse::success('Oferta cadastrada com sucesso', ['offer_id' => $offer->id]);
--- backend/app/Http/Controllers/SearchController.php
+++ backend/app/Http/Controllers/SearchController.php
@@ -3,13 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Actions\SearchProductsAction;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use App\Helpers\ApiResponse;
-use Illuminate\Support\Arr;
 
 class SearchController extends Controller
 {
-    public function search(Request $request, SearchProductsAction $action)
+    public function search(Request $request, SearchProductsAction $action): JsonResponse
     {
         $query = $request->input('query');
 
@@ -23,9 +23,6 @@ class SearchController extends Controller
             return ApiResponse::error('Nenhum produto encontrado', 404);
         }
 
-        return ApiResponse::success(
-           'Resultados',
-            $data
-        );
+        return ApiResponse::success('Resultados', $data);
     }
 }
