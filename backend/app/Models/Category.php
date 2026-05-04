607cf93bf844b17bfd96cbd73a46161cd781c33f
Djalma Manfrin <djalma.manfrin@gmail.com>
2026-05-04 17:14:52 -0300

    Adiciona relações `parent` e `children` ao modelo `Category`.

--- backend/app/Models/Category.php
+++ backend/app/Models/Category.php
@@ -3,13 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
     protected $fillable = ['name', 'normalized_name'];
 
-    public function products()
+    public function products(): HasMany
     {
         return $this->hasMany(Product::class);
+    }
+    public function parent(): BelongsTo
+    {
+        return $this->belongsTo(Category::class, 'parent_id');
+    }
+
+    public function children(): HasMany
+    {
+        return $this->hasMany(Category::class, 'parent_id');
     }
 }
