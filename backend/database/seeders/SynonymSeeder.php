e004fdd090ef8973487df82df378b952b45393fd
Djalma Manfrin <djalma.manfrin@gmail.com>
2026-05-14 17:33:24 -0300

    Adiciona novos sinônimos e categorias ao `SynonymSeeder` e ajusta tipos de `unit` para `volume`.

--- backend/database/seeders/SynonymSeeder.php
+++ backend/database/seeders/SynonymSeeder.php
@@ -13,7 +13,7 @@ class SynonymSeeder extends Seeder
 
             /*
             |--------------------------------------------------------------------------
-            | MARCAS
+            | Brands
             |--------------------------------------------------------------------------
             */
 
@@ -22,16 +22,24 @@ class SynonymSeeder extends Seeder
             ['term' => 'cocacola',    'normalized' => 'coca cola', 'type' => 'brand'],
             ['term' => 'coka',        'normalized' => 'coca cola', 'type' => 'brand'],
 
+            ['term' => 'pepsi','normalized' => 'pepsi','type' => 'brand'],
+
             ['term' => 'guarana',     'normalized' => 'guarana', 'type' => 'brand'],
-            ['term' => 'guaraná',     'normalized' => 'guarana', 'type' => 'brand'],
             ['term' => 'antarctica',  'normalized' => 'guarana', 'type' => 'brand'],
 
+            /*
+            |--------------------------------------------------------------------------
+            | Flavores
+            |--------------------------------------------------------------------------
+            */
+            ['term' => 'cola','normalized' => 'cola','type' => 'flavor'],
+            ['term' => 'laranja','normalized' => 'laranja','type' => 'flavor'],
             /*
             |--------------------------------------------------------------------------
             | CATEGORY
             |--------------------------------------------------------------------------
             */
-
+            ['term' => 'refrigerante', 'normalized' => 'refrigerante', 'type' => 'category'],
             ['term' => 'refri',       'normalized' => 'refrigerante', 'type' => 'category'],
             ['term' => 'refr',        'normalized' => 'refrigerante', 'type' => 'category'],
 
@@ -40,18 +48,14 @@ class SynonymSeeder extends Seeder
             | EMBALAGEM (unit_type)
             |--------------------------------------------------------------------------
             */
-
             ['term' => 'pet',         'normalized' => 'garrafa', 'type' => 'packaging'],
             ['term' => 'longneck',    'normalized' => 'garrafa', 'type' => 'packaging'],
             ['term' => 'long neck',   'normalized' => 'garrafa', 'type' => 'packaging'],
             ['term' => 'ln',          'normalized' => 'garrafa', 'type' => 'packaging'],
-
             ['term' => 'latinha',     'normalized' => 'lata', 'type' => 'packaging'],
             ['term' => 'latao',       'normalized' => 'lata', 'type' => 'packaging'],
-
             ['term' => 'cx',          'normalized' => 'caixa', 'type' => 'packaging'],
             ['term' => 'caixinha',    'normalized' => 'caixa', 'type' => 'packaging'],
-
             ['term' => 'pct',         'normalized' => 'pacote', 'type' => 'packaging'],
 
             /*
@@ -61,19 +65,19 @@ class SynonymSeeder extends Seeder
             */
 
             // volume
-            ['term' => 'litro',       'normalized' => 'l', 'type' => 'unit'],
-            ['term' => 'litros',      'normalized' => 'l', 'type' => 'unit'],
-            ['term' => 'ml',          'normalized' => 'ml', 'type' => 'unit'],
-            ['term' => 'mililitro',   'normalized' => 'ml', 'type' => 'unit'],
-            ['term' => 'mililitros',  'normalized' => 'ml', 'type' => 'unit'],
+            ['term' => 'litro',       'normalized' => 'l', 'type' => 'volume'],
+            ['term' => 'litros',      'normalized' => 'l', 'type' => 'volume'],
+            ['term' => 'ml',          'normalized' => 'ml', 'type' => 'volume'],
+            ['term' => 'mililitro',   'normalized' => 'ml', 'type' => 'volume'],
+            ['term' => 'mililitros',  'normalized' => 'ml', 'type' => 'volume'],
 
             // peso
-            ['term' => 'quilo',       'normalized' => 'kg', 'type' => 'unit'],
-            ['term' => 'quilos',      'normalized' => 'kg', 'type' => 'unit'],
-            ['term' => 'kg',          'normalized' => 'kg', 'type' => 'unit'],
-            ['term' => 'grama',       'normalized' => 'g', 'type' => 'unit'],
-            ['term' => 'gramas',      'normalized' => 'g', 'type' => 'unit'],
-            ['term' => 'g',           'normalized' => 'g', 'type' => 'unit'],
+            ['term' => 'quilo',       'normalized' => 'kg', 'type' => 'volume'],
+            ['term' => 'quilos',      'normalized' => 'kg', 'type' => 'volume'],
+            ['term' => 'kg',          'normalized' => 'kg', 'type' => 'volume'],
+            ['term' => 'grama',       'normalized' => 'g', 'type' => 'volume'],
+            ['term' => 'gramas',      'normalized' => 'g', 'type' => 'volume'],
+            ['term' => 'g',           'normalized' => 'g', 'type' => 'volume'],
 
             /*
             |--------------------------------------------------------------------------
